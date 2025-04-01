152519d4b15ab1f3c704cc8c230a64432281f537
KhoaTr197 <darkreaper197@gmail.com>
2025-04-01 22:51:43 +0700

    Add 2 Product/User Manager Form GUI:
    + Has their own BUS layer property, LoadData method
    
    That's it!

---
+++ TechForgeGUI/SubForms/ProductManageFormGUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TechForgeGUI.BaseForms;
+using TechForgeBUS;
+
+namespace TechForgeGUI
+{
+  public partial class ProductManageFormGUI : ManageFormGUI
+  {
+    private SanPhamBUS bus { get; set; }
+    public ProductManageFormGUI()
+    {
+      InitializeComponent();
+      InitializeBUS();
+      LoadData();
+    }
+    sealed protected override void InitializeBUS()
+    {
+      bus = new SanPhamBUS(this.connStr);
+    }
+    sealed protected override void LoadData()
+    {
+      dgvMainListRef.DataSource = bus.GetAllDisconnected().Tables["SANPHAM"];
+    }
+  }
+}
---
+++ TechForgeGUI/SubForms/UserManagerFormGUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TechForgeBUS;
+using TechForgeGUI.BaseForms;
+
+namespace TechForgeGUI.SubForms
+{
+  public partial class UserManagerFormGUI : ManageFormGUI
+  {
+    private NguoiDungBUS bus { get; set; }
+    public UserManagerFormGUI()
+    {
+      InitializeComponent();
+      InitializeBUS();
+      LoadData();
+    }
+    protected override void InitializeBUS()
+    {
+      bus = new NguoiDungBUS(this.connStr);
+    }
+    protected override void LoadData()
+    {
+      dgvMainListRef.DataSource = bus.GetAllConnected();
+    }
+  }
+}
